228b6805ed92f909008d2d57df4c9d20d35d8cfd
OleksandraZakovorotna <x.boogieboo.x@gmail.com>
2026-03-13 10:37:30 +0200

    TS task 1-8

---
+++ src/task-5.ts
@@ -0,0 +1,13 @@
+type LoadStatus = "loading" | "success" | "error";
+
+function logStatus(status: LoadStatus) {
+  if (status === "loading") {
+    console.log("Loading...");
+  } else if (status === "success") {
+    console.log("Success!");
+  } else if (status === "error") {
+    console.log("Something went wrong");
+  }
+}
+
+logStatus("loading");
